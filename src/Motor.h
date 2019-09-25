442a38a00e8a639984ed368e92e0917139ad1556
Pascal Canuel <pascalcanuel@hotmail.com>
2019-09-25 14:53:02 -0400

    cleaner

--- src/Motor.h
+++ src/Motor.h
@@ -6,39 +6,38 @@
 enum MotorId {
     LEFT_MOTOR = 0,
     RIGHT_MOTOR = 1,
-    BOTH_MOTOR = 2,
 };
 
 struct Motor
 {
-    Motor() {
-        resetEncoder(BOTH_MOTOR);
+    MotorId _motorId;
+
+    Motor() {}
+    Motor(MotorId motorId) {
+        _motorId = motorId;
+
+        resetEncoder();
     }
 
-    void setSpeed(MotorId motorId, float speed) {
-        if(motorId == BOTH_MOTOR) {
-            MOTOR_SetSpeed(LEFT_MOTOR, speed);
-            MOTOR_SetSpeed(RIGHT_MOTOR, speed);
-        } else {
-            MOTOR_SetSpeed(motorId, speed);
-        }
+    void resetEncoder() {
+        ENCODER_Reset(_motorId);
     }
 
-    void resetEncoder(MotorId motorId) {
-        if(motorId == BOTH_MOTOR) {
-            ENCODER_Reset(LEFT_MOTOR);
-            ENCODER_Reset(RIGHT_MOTOR);
-        } else {
-            ENCODER_Reset(motorId);
-        }
+    int32_t readResetEncoder() {
+        return ENCODER_ReadReset(_motorId);
     }
 
-    int32_t readEncoder(MotorId motorId) {
-        if(motorId == BOTH_MOTOR) {
-            Serial.println("Can't read both encoder at the same time.");
-            return 0;
-        }
-        return ENCODER_Read(motorId);
+    int32_t readEncoder() {
+        return ENCODER_Read(_motorId);
+    }
+
+    void setSpeed(float speed) {
+        MOTOR_SetSpeed(_motorId, speed);
+    }
+
+    void stop() {
+        setSpeed(0);
+        resetEncoder();
     }
 };
 
--- src/Robot.h
+++ src/Robot.h
@@ -1,59 +1,45 @@
 #ifndef Robot_H_
 #define Robot_H_
 
+#include "Robot.Utils.h"
 #include "Motor.h"
-
-const float ROBUS_WIDTH = 18.75;
-const float ROBUS_CIRCUMFERENCE = 2*PI*ROBUS_WIDTH;
-
-const float WHEEL_RADIUS = 3.81;
-const float WHEEL_CIRCUMFERENCE = 2*PI*WHEEL_RADIUS;
-const float WHEEL_TOUR_PULSE = 3200;
-
-const float DEFAULT_SPEED = 0.2;
 
 struct Robot
 {
-    Motor _motor;
+    Motor _leftMotor;
+    Motor _rightMotor;
 
     Robot() {
         BoardInit();
-        _motor = Motor();
+        _leftMotor = Motor(LEFT_MOTOR);
+        _rightMotor = Motor(RIGHT_MOTOR);
 
         delay(1000);
     }
 
-    int32_t convertDegreeToDistance(float degree) {
-        return abs((degree/360) * ROBUS_CIRCUMFERENCE);
-    }
-
-    int32_t convertDistanceToPulse(float distance) {
-        return abs((distance/WHEEL_CIRCUMFERENCE) * WHEEL_TOUR_PULSE);
-    }
-
     void rotate(float degree) {
-        MotorId motorId = LEFT_MOTOR;
+        Motor motor = _leftMotor;
         if(degree > 0) 
-            motorId = RIGHT_MOTOR;
+            motor = _rightMotor;
 
         float distanceToReach = convertDegreeToDistance(degree);
         float pulseToReach = convertDistanceToPulse(distanceToReach);
 
-        _motor.setSpeed(motorId, DEFAULT_SPEED);
-        while (_motor.readEncoder(motorId) < pulseToReach) {}
+        motor.setSpeed(DEFAULT_SPEED);
+        while (motor.readEncoder() < pulseToReach) {}
 
-        _motor.setSpeed(motorId, 0);
-        _motor.resetEncoder(motorId);
+        motor.stop();
     }
 
     void move(float distance) {
         float pulseToReach = convertDistanceToPulse(distance);
 
-        _motor.setSpeed(BOTH_MOTOR, DEFAULT_SPEED);
-        while (_motor.readEncoder(LEFT_MOTOR) < pulseToReach && _motor.readEncoder(RIGHT_MOTOR) < pulseToReach) {}
+        _leftMotor.setSpeed(DEFAULT_SPEED);
+        _rightMotor.setSpeed(DEFAULT_SPEED);
+        while (_leftMotor.readEncoder() < pulseToReach && _rightMotor.readEncoder() < pulseToReach) {}
         
-        _motor.setSpeed(BOTH_MOTOR, 0);
-        _motor.resetEncoder(BOTH_MOTOR);
+        _leftMotor.stop();
+        _rightMotor.stop();
     }
 };
 
