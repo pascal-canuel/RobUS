2fc908b1964e0b2899fde62395f8482c7cbf295c
Pascal Canuel <pascalcanuel@hotmail.com>
2019-09-26 10:48:38 -0400

    wtf

--- src/Robot.h
+++ src/Robot.h
@@ -2,21 +2,19 @@
 #define Robot_H_
 
 #include "Robot.Utils.h"
-#include "PID.h"
 #include "Motor.h"
 
 struct Robot
 {
     Motor _leftMotor;
     Motor _rightMotor;
-    PID _pidMotor;
 
     Robot() {
         BoardInit();
         _leftMotor = Motor(LEFT_MOTOR);
         _rightMotor = Motor(RIGHT_MOTOR);
 
-        delay(1000);
+        delay(2000);
     }
 
     void rotate(float degree) {
@@ -35,6 +33,7 @@ struct Robot
 
     void move(float distance) {
         float pulseToReach = convertDistanceToPulse(distance);
+        Serial.println(pulseToReach);
 
         _leftMotor.setSpeed(DEFAULT_SPEED);
         _rightMotor.setSpeed(DEFAULT_SPEED);
@@ -45,9 +44,6 @@ struct Robot
         {
             leftPulse = _leftMotor.readEncoder();
             rightPulse = _rightMotor.readEncoder();
-
-            float magic = _pidMotor.Compute(leftPulse, rightPulse);
-            _rightMotor.addSpeed(magic);
         } while (leftPulse < pulseToReach && rightPulse < pulseToReach);
         
         _leftMotor.stop();
