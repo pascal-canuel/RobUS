f2faa03e4c2d4f40918e3efe16f6519be3e09101
Pascal Canuel <Pascal.Canuel@USherbrooke.ca>
2019-10-03 10:45:23 -0400

    Dev/inverse (#3)
    * parcours
    
    * move backward
    
    * reverse looks good
    
    * nani omawaiiii mou shinderouuu
    
    * wrap motor & robot into structure
    
    * pid template
    
    * pid template

--- src/Robot.Utils.h
+++ src/Robot.Utils.h
@@ -10,7 +10,7 @@ const float WHEEL_RADIUS = 3.81;
 const float WHEEL_CIRCUMFERENCE = 2*PI*WHEEL_RADIUS;
 const float WHEEL_TOUR_PULSE = 3200;
 
-const float DEFAULT_SPEED = 0.2;
+const float DEFAULT_SPEED = 0.3;
 
 static int32_t convertDegreeToDistance(float degree) {
     return abs((degree/360) * ROBUS_CIRCUMFERENCE);
