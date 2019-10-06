f3fc3c9bc80cb958a64ed12ee29d59c0f773b5de
MissMarchal <eloise.marchal204@gmail.com>
2019-10-06 14:21:46 -0400

    allo

--- src/main.ino
+++ src/main.ino
@@ -9,8 +9,8 @@ Date: Derniere date de modification
 #include "Path.h"
 
 Robot robus;
-Path path;
-int length;
+Path path, path2;
+int length,lenght2;
 
 void setup(){
   BoardInit();
@@ -18,25 +18,45 @@ void setup(){
   robus = Robot();
   robus.reset();
   Step steps[] = {
-    {MOVE, 115},
-    {ROTATE, 90},
-    {MOVE, 70},
-    {ROTATE, -90},
-    {MOVE, 70},
-    {ROTATE, -45},
-    {MOVE, 185},
-    {ROTATE, 90},
-    {MOVE, 45},
-    {ROTATE, -45},
-    {MOVE, 110}
+    {MOVE, 120},
+    {TURN, 90},
+    {MOVE, 90},
+    {TURN, -90},
+    {MOVE, 90},
+    {TURN, -45},
+    {MOVE, 183},
+    {TURN, 90},
+    {MOVE, 50},
+    {TURN, -45},
+    {MOVE, 135}
   };
   length = 11;
   path = Path(robus, steps);
+
+  robus.reset();
+  Step steps2[] = {
+    {MOVE, 135},
+    {TURN, 45},
+    {MOVE, 50},
+    {TURN, -90},
+    {MOVE, 185},
+    {TURN, -45},
+    {MOVE, 70},
+    {TURN, -90},
+    {MOVE, 70},
+    {TURN, 90},
+    {MOVE, 115}
+  };
+  lenght2 = 11;
+  path2 = Path(robus, steps2);
 }
 
 void loop() {
   if (ROBUS_IsBumper(3)) {
     path.execute(length);
+    //robus.turn(180);
     path.reverse(length);
+    //path2.execute(lenght2);
+   
   }
 }
