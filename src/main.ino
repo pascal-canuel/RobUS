1d5c70c788875cdec5a705194e9d22dbd49cc366
Pascal Canuel <pascalcanuel@hotmail.com>
2019-10-28 00:03:17 -0400

    fix and clean

--- src/main.ino
+++ src/main.ino
@@ -18,7 +18,7 @@ Auteurs:
 
 Robot robus;
 
-Adafruit_TCS34725 tcs = Adafruit_TCS34725();
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_4X);
 
 enum Color {
   YELLOW, 
@@ -31,6 +31,11 @@ enum Color {
 struct Rgb {
   uint16_t red, green, blue;
 };
+
+// Rgb redRgb { 254, 0, 0 };
+// Rgb greenRgb { 0, 255, 0 };
+// Rgb blueRgb { 0, 0, 255 };
+// Rgb yellowRgb { 255, 255, 0 };
 
 void setup(){
   BoardInit();
@@ -46,20 +51,20 @@ void setup(){
 }
 
 void loop() {
-  //uint16_t red, green, blue, clear;
-  //tcs.getRawData(&red, &green, &blue, &clear);
+  // uint16_t red, green, blue, clear;
+  // tcs.getRawData(&red, &green, &blue, &clear);
 
-  float red, green, blue, clear;
+  float red, green, blue;
   tcs.getRGB(&red, &green, &blue);
 
-  Serial.print("red"); Serial.println(red);
-  Serial.print("green"); Serial.println(green);
-  Serial.print("blue"); Serial.println(blue);
-
-  Rgb rgb { red, green, blue };
-  Color color = rgbToColor(rgb);
-
-  Serial.println(color);
+  if (red == green && red == blue && green == blue) {
+    Serial.println("error with the captor");
+  } else {
+    Rgb rgb { red, green, blue };
+    Color color = rgbToColor(rgb);
+    Serial.println("red | green | blue | color");
+    Serial.print(red); Serial.print(" | "); Serial.print(green); Serial.print(" | "); Serial.print(blue); Serial.print(" | "); Serial.println(color);
+  }
   delay(2000);
 }
 
@@ -74,16 +79,18 @@ float rgbToHue(Rgb rgb) {
   float delta = cMax - cMin;
   float hue = 0;
 
-  if (blue == cMax) {
-    hue = 60 * ((red - green) / delta + 4);
-  }
-  else if (green == cMax) {
-    hue = 60 * ((blue - red) / delta + 2);
-  }
-  else if (red == cMax) {
-    hue = 60 * ((green - blue) / delta);	
-    if (hue < 0)
-      hue += 360;
+  if (delta != 0) {
+    if (blue == cMax) {
+      hue = 60 * ((red - green) / delta + 4);
+    }
+    else if (green == cMax) {
+      hue = 60 * ((blue - red) / delta + 2);
+    }
+    else if (red == cMax) {
+      hue = 60 * ((green - blue) / delta);	
+      if (hue < 0)
+        hue += 360;
+    }
   }
 
   return hue / 2;
@@ -99,13 +106,13 @@ float rgbToHue(Rgb rgb) {
   Red 160 - 179
 */
 Color hueToColor(float hue) {
-  if (22 < hue <= 38)
+  if (22 < hue && hue <= 38)
     return YELLOW;
-  if (38 < hue <= 75) 
+  if (38 < hue && hue <= 75) 
     return GREEN;
-  if (75 < hue <= 130)
+  if (75 < hue && hue <= 130)
     return BLUE;
-  if (160 < hue <= 179)
+  if ((0 <= hue && hue <= 22) || (160 < hue && hue <= 179))
     return RED;
   
   return UNDEFINED;
