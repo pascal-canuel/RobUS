30e4fd2ddfd841e83b8ba88d44bbd895c26107ae
Pascal Canuel <pascalcanuel@hotmail.com>
2019-11-01 19:05:56 -0400

    template

--- src/main.ino
+++ src/main.ino
@@ -20,13 +20,41 @@ void setup(){
 
   robus = Robot();
   robus.init();
+  
+  /*
+    Robus A - go get the ball on a color zone and place it in the middle
+    Robus B - get the ball in the middle and place it on a color zone
+  */
+  #ifdef ROBUS_A
+    // 1. Go on a line and move to a color zone 
+    // 2. When on a color zone calculate fastest path to the given color zone
+    // 3. Go in the middle
+    // 4. Leave the ball there
+    // 5. Move back as not to block the Robus B who takes the ball
+  #else
+    // 1. Move forward to get ball in the middle
+    // 2. Grab the ball
+    // 2. Go on a line and move to a color zone 
+    // 3. When on a color zone calculate fastest path to the given color zone
+    // 4. Go in the given color zone
+    // 5. Leave the ball there
+  #endif
 }
 
 void loop() {
-  Color color = robus.readColor();
-  Serial.print("color: "); Serial.println(color);
+  /*
+    0 - yellow
+    1 - green
+    2 - blue
+    3 - red
+    4 - undefined
+  */
+  // Color color = robus.readColor();
+  // Serial.print("color: "); Serial.println(color);
   
-  // robus.followLine();
+  // if (ROBUS_IsBumper(3)) {
+  //  robus.followLine();
+  // }
 
   delay(500);
 }
