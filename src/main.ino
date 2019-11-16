bbebf75e442da3f77b1d81b71bd435bf29e7dad4
Pascal Canuel <pascalcanuel@hotmail.com>
2019-11-16 11:55:50 -0500

    rfid works

--- src/main.ino
+++ src/main.ino
@@ -10,13 +10,13 @@ Auteurs:
   - Xavier Champagne
 */
 
+#include "robot/Robot.h"
+
 #define ROBUS_A 0
 #define ROBUS_B 1
 #define __ROBUS__ ROBUS_A
 
-#include "robot/Robot.h"
-
-#define SerialRFID Serial1
+#define SerialRFID Serial1 // 19 (RX1)
 
 void setup()
 {
@@ -27,15 +27,63 @@ void setup()
   SerialRFID.begin(9600);
 }
 
-void loop() {
-  if (SerialBT.available()) {
-    String str = SerialBT.readString();
-    if (str == "MOVE") {
+// void loop() {
+//   if (SerialBT.available()) {
+//     String str = SerialBT.readString();
+//     if (str == "MOVE") {
 
-    } else if (str == "FOOD") {
+//     } else if (str == "FOOD") {
 
-    } else if (str == "WHISTLE") {
+//     } else if (str == "WHISTLE") {
 
+//     }
+//   }
+// }
+
+char crecu, incoming=0;
+String idTag;
+
+String tags[][2] = {
+  {"0E008E637F", "RFTC014"},
+  {"0E008E6F32", "RFTC015"},
+  {"0E008E9C42", "RFTC016"}
+};
+
+void loop()
+{
+  while(1)
+  {
+    if(SerialRFID.available())
+    {
+
+      crecu = SerialRFID.read(); // lit le ID-12
+      switch(crecu)
+      {
+        case 0x02:
+          // START OF TRANSMIT
+          AX_BuzzerON();
+          idTag = "";
+          incoming = 1;
+          break;
+        case 0x03:
+          // END OF TRANSMIT
+          AX_BuzzerOFF();
+          incoming = 0;
+          // Affiche le code recu sans valider le checksum
+          idTag = idTag.substring(0, 10);
+          for(auto& tag: tags)
+          {
+            if (tag[0] == idTag)
+            {
+              Serial.println(tag[1]);
+            }
+          }
+          break;
+        default:
+          if(incoming)
+            idTag += crecu;
+          break;
+      }
     }
   }
 }
